960fd1fd55e8500ca8ef98ee994d1edf42d58e22
Willhs <whardwicksmith@gmail.com>
2015-06-17 17:20:17 +1200

    started visuals

--- gui.js
+++ gui.js
@@ -5,18 +5,13 @@
 	var onReady = function (data) {
 		var svg = d3.select("body").append("svg")
 					.attr("width", 500)
-					.attr("height", 500);
+					.attr("height", 500)
+                    .attr("transform", "translate(0,0)");
 
-		console.log(data[10]);
-
-		data.map(function (entry) {
-			if(entry.byes) {
-				console.log(entry);
-			}
-		});
+        netball.visuals.scoreVis(data, svg);
 	}
 
-	//calls onReady with the data once ready.
+	//calls onRead with the data once ready.
 	netball.parser.loadData().then(function (data) {
 		onReady(data);
 	});
